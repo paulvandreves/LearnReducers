e3b729d64b665c7b5b276888440d82fa957afb96
Paul  Van Dyke <paulvandyke@Pauls-MacBook-Air.local>
2021-11-04 19:33:18 -0400

    Learning about use reducer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
 
+// Tutorial referenced: https://www.robinwieruch.de/react-usereducer-hook
+
+const initialTodos = [
+  {
+    id: 'a',
+    task: 'Learn React',
+    complete: false,
+  },
+  {
+    id: 'b',
+    task: 'Learn Firebase',
+    complete: false,
+  },
+];
+
+const todoReducer = (state, action) => {
+  switch (action.type) {
+    case 'DO_TODO':
+      return state.map(todo => {
+        if (todo.id === action.id) {
+          return { ...todo, complete: true };
+        } else {
+          return todo;
+        }
+      });
+    case 'UNDO_TODO':
+      return state.map(todo => {
+        if (todo.id === action.id) {
+          return { ...todo, complete: false };
+        } else {
+          return todo;
+        }
+      });
+    default:
+      return state;
+  }
+};
+
+const [todos, dispatch] = useReducer(todoReducer, initialTodos);
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+        
+     
     </div>
   );
 }
